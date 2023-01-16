c422088a1b1b0439ed2fee29f4a6bb299f949b2b
Bilge Ergün <blgergn@gmail.com>
2023-01-16 23:47:42 +0300

    Initial commit

---
+++ src/day31_Constructors/PersonObject.java
@@ -0,0 +1,13 @@
+package day31_Constructors;
+
+public class PersonObject {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Bilge", 'F', 31);
+
+        System.out.println(person1);
+
+
+    }
+}
