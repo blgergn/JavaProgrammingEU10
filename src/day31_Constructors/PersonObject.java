fad2e280a3f6124aac97ac6e2c4360f2ec2e523a
Bilge Ergün <blgergn@gmail.com>
2023-01-17 00:44:03 +0300

    day31 Constructors

--- src/day31_Constructors/PersonObject.java
+++ src/day31_Constructors/PersonObject.java
@@ -5,8 +5,12 @@ public class PersonObject {
     public static void main(String[] args) {
 
         Person person1 = new Person("Bilge", 'F', 31);
+        Person person2= new Person("Julide",'F', 32);
+
+        person2.age=34;
 
         System.out.println(person1);
+        System.out.println(person2);
 
 
     }
---
+++ src/day31_Constructors/StudentObjects.java
@@ -0,0 +1,16 @@
+package day31_Constructors;
+
+public class StudentObjects {
+
+    public static void main(String[] args) {
+
+        Student student1=new Student("Bilge",30,'F','A',112233);
+        Student student2=new Student("Julide", 34,'F', 'A', 334455);
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+
+
+    }
+}
